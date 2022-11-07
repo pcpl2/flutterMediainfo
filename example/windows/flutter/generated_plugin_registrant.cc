d1282cdd5651d0956c709111512285cc1d1a85de
Patryk Ławicki <patryk.lawicki@iqtech.pl>
2022-11-07 10:57:59 +0100

    Updated dependencies for library and example

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -6,12 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <file_selector_windows/file_selector_plugin.h>
+#include <file_selector_windows/file_selector_windows.h>
 #include <flutter_media_info/flutter_media_info_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  FileSelectorPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FileSelectorPlugin"));
+  FileSelectorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileSelectorWindows"));
   FlutterMediaInfoPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterMediaInfoPlugin"));
 }
