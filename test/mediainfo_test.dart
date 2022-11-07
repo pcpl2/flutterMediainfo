aaa13ec921707ec04dc80612ac71549ba0b81ac9
Patryk Ławicki <patryk.lawicki@iqtech.pl>
2022-11-07 11:24:25 +0100

    Updated libMediaInfo from v21.09 to v22.09

--- test/mediainfo_test.dart
+++ test/mediainfo_test.dart
@@ -8,83 +8,83 @@ import "package:path/path.dart" as path;
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  late String _debugPath;
+  late String debugPath;
 
   if (Platform.isLinux) {
-    _debugPath = "linux/libs/";
+    debugPath = "linux/libs/";
   }
   if (Platform.isMacOS) {
-    _debugPath = "macos/libs/";
+    debugPath = "macos/libs/";
   }
   if (Platform.isWindows) {
-    _debugPath = "windows/libs/";
+    debugPath = "windows/libs/";
   }
 
   test('load mediainfo', () async {
-    Mediainfo(customDebugPath: _debugPath);
+    Mediainfo(customDebugPath: debugPath);
   });
 
   test('mediainfo get version', () async {
-    final _mi = Mediainfo(customDebugPath: _debugPath);
-    final optionResult = _mi.option("Info_Version");
+    final mi = Mediainfo(customDebugPath: debugPath);
+    final optionResult = mi.option("Info_Version");
 
-    expect(optionResult, "MediaInfoLib - v21.09");
+    expect(optionResult, "MediaInfoLib - v22.09");
   });
 
   test('Get music data with quick load', () async {
     final musicPath = path.join(Directory.current.path, "assets/Beat_Thee.mp3");
-    final _mi = Mediainfo(customDebugPath: _debugPath);
-    _mi.quickLoad(musicPath);
+    final mi = Mediainfo(customDebugPath: debugPath);
+    mi.quickLoad(musicPath);
     expect(
-        _mi.getInfo(
+        mi.getInfo(
             MediaInfoStreamType.mediaInfoStreamAudio, 0, "BitRate/String"),
         "320 kb/s");
     expect(
-        _mi.getInfo(
+        mi.getInfo(
             MediaInfoStreamType.mediaInfoStreamAudio, 0, "Duration/String2"),
         "3 min 11 s");
     expect(
-        _mi.getInfo(
+        mi.getInfo(
             MediaInfoStreamType.mediaInfoStreamAudio, 0, "SamplingRate/String"),
         "44.1 kHz");
     expect(
-        _mi.getInfo(
+        mi.getInfo(
             MediaInfoStreamType.mediaInfoStreamAudio, 0, "FrameRate/String"),
         "38.281 FPS (1152 SPF)");
 
-    _mi.delete();
+    mi.delete();
   });
 
   test('Get music data with open', () async {
     final musicPath = path.join(Directory.current.path, "assets/Beat_Thee.mp3");
-    final _mi = Mediainfo(customDebugPath: _debugPath);
-    _mi.init();
-    _mi.open(musicPath);
+    final mi = Mediainfo(customDebugPath: debugPath);
+    mi.init();
+    mi.open(musicPath);
     expect(
-        _mi.getInfo(
+        mi.getInfo(
             MediaInfoStreamType.mediaInfoStreamAudio, 0, "BitRate/String"),
         "320 kb/s");
     expect(
-        _mi.getInfo(
+        mi.getInfo(
             MediaInfoStreamType.mediaInfoStreamAudio, 0, "Duration/String2"),
         "3 min 11 s");
     expect(
-        _mi.getInfo(
+        mi.getInfo(
             MediaInfoStreamType.mediaInfoStreamAudio, 0, "SamplingRate/String"),
         "44.1 kHz");
     expect(
-        _mi.getInfo(
+        mi.getInfo(
             MediaInfoStreamType.mediaInfoStreamAudio, 0, "FrameRate/String"),
         "38.281 FPS (1152 SPF)");
 
-    _mi.close();
-    _mi.delete();
+    mi.close();
+    mi.delete();
   });
 
   test('Invalid usage close', () async {
-    final _mi = Mediainfo(customDebugPath: _debugPath);
+    final mi = Mediainfo(customDebugPath: debugPath);
 
-    _mi.close();
-    _mi.delete();
+    mi.close();
+    mi.delete();
   });
 }
