0493b3fa4cfc1b2877c874e54da5344e50331ecf
Patryk Ławicki <patryk.lawicki@iqtech.pl>
2023-02-14 16:37:20 +0100

    Documentation has been supplemented

--- lib/src/models/media_info_exceptions.dart
+++ lib/src/models/media_info_exceptions.dart
@@ -1,11 +1,13 @@
+/// Exception thrown when media info instance is not exist 
 class NotLoadedMediaInfoInstance implements Exception {
   NotLoadedMediaInfoInstance();
 }
-
+/// Exception thrown when media info instance is exist
 class MediaInfoInstanceHasExists implements Exception {
   MediaInfoInstanceHasExists();
 }
 
+/// Exception thrown when file is not loaded
 class FileHasNotLoaded implements Exception {
   FileHasNotLoaded();
 }
--- lib/src/models/media_info_stream_type.dart
+++ lib/src/models/media_info_stream_type.dart
@@ -1,10 +1,20 @@
+
+/// Media info Stream types
 enum MediaInfoStreamType {
+  // General data | StreamKind = General.
   mediaInfoStreamGeneral,
+  // Video data | StreamKind = Video.
   mediaInfoStreamVideo,
+  // Audio data | StreamKind = Audio.
   mediaInfoStreamAudio,
+  // Subtitles / Text data | StreamKind = Text.
   mediaInfoStreamText,
+  // Other data | StreamKind = Chapters.
   mediaInfoStreamOther,
+  // Image data | StreamKind = Image.
   mediaInfoStreamImage,
+  // Menu data | StreamKind = Menu.
   mediaInfoStreamMenu,
+  // Count of elements in c++ //TODO Remove it
   mediaInfoStreamMax,
 }
